a07a2f51b175792c1ef08e1ac1123596c6a72f27
Beto Esquivel <beto.esquivel.p@gmail.com>
2014-02-16 21:12:38 -0600

    Hice codigo bash que hace lista de palabras diferentes y su frecuencia de aparicion en el texto, utilizando grep... se tarda un poco, pero ni modo

--- palabras.sh
+++ palabras.sh
@@ -13,30 +13,35 @@ archivoEntrada=$1
 #guardo el argumento con el nombre del archivo salida en archivoSalida
 archivoSalida=$2
 
-#imprimo mis dos arreglos en el archivo de salida
-# imprimirSalida()
-# {
-# 
-# }
+#variable que me sirve como archivo temporal
+archivoTemporal="temp.txt"
 
 palabras=""
-regex="palabras([:punct:])?"
+regex="([:punct:])?"
 resultado=""
+
+cat $archivoEntrada | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' > $archivoTemporal
+
+#borro contenidos archivo salida
+echo "" > $archivoSalida
+
 while read -r line
 do
     for word in $line; do
-	    echo $word
-	    shopt -s nocasematch
-	    if [[ "$word" =~ $regex ]]; then
-		    echo "Ya esta en mi string."
-	    else
-		    echo "Agregando al string..."
-		    palabras+="$word 1 "
-	    fi
+		resultado=""
+		resultado=`echo $palabras | grep -o $word`
+		if [[ $resultado != "" ]]; then
+			resultado=""
+		else
+			resultado=`cat $archivoTemporal | grep -o $word | wc -l`
+			resultado+=" $word" 
+			palabras+="$resultado"
+			#imprimo el resultado en el archivo salida
+			echo $resultado >> $archivoSalida
+		fi
     done
-done < "$archivoEntrada"
+done < $archivoTemporal 
 #leo del archivo texto3.txt
 
-echo "$palabras"
 
 
